802d3de1b882be8d0ad2e50cfd0e715fba65d204
theGeekkit <sonofgondolin16@gmail.com>
2023-07-27 13:54:23 -0500

    7-27-23

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+// Console.WriteLine("Hello, World!");
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Heath";
+            bool isWorking = true;
+            Console.WriteLine(number);
+        }
+    }
+}
